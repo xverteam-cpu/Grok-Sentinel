347c5345dde2dec03930c6104baf6bea6795fdf7
leandromarucut3-oss <xver.team@gmail.com>
2026-04-10 09:54:20 +0800

    Update favicon to Sentinel-logo.png

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="/Sentinel-logo.png">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
